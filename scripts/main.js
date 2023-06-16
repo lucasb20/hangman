ac1c8021c3a9d36994e939d0ab66977716212ec7
Lucas Rocha <lr359508@gmail.com>
2023-06-15 22:18:35 -0300

    Projeto finalizado.

--- scripts/main.js
+++ scripts/main.js
@@ -14,22 +14,53 @@ const sub_posx = 100,sub_posy = 350
 const gallows_posx = 25, gallows_posy = 50
 const hangman_posx = 180, hangman_posy = 110
 
+const check_posx = 275,check_posy=100
+
+const but_send = document.querySelector('#but_send')
+but_send.addEventListener('click',send)
+
 function init(){
     chosen_word = words[Math.floor(Math.random()*words.length)]
+    for(let i=0;i<chosen_word.length;i++)chars.push('')
     update()
 }
 
 function update(){
+    ctx.clearRect(0,0,canvas.width,canvas.height)
     draw_gallows()
     ger_word()
-    num_error = 6
-    draw_hangman(num_error)
+    ctx.font = "50px arial black"
+    for(let i=0;i<chars.length;i++){
+        ctx.fillText(chars[i],sub_posx+5+i*60,sub_posy-5)
+    }
+    check_win(draw_hangman(num_error))
+}
+
+function check_win(num){
+    if(num == 1){
+        ctx.font = '40px arial black'
+        ctx.fillText('Você perdeu.',check_posx,check_posy)
+        but_send.removeEventListener('click',send)
+
+        return 0
+    }
+
+    let count=0
+    for(let i=0;i<chars.length;i++){
+        if(chars[i]!='')count++
+    }
+
+    if(count == chars.length){
+        ctx.font = '40px arial black'
+        ctx.fillText('Você ganhou.',check_posx,check_posy)
+        but_send.removeEventListener('click',send)
+    }
 }
 
 function ger_word(){
     ctx.font = "100px arial black"
     for(let i=0;i<chosen_word.length;i++){
-        ctx.fillText('_ ',sub_posx+i*60,sub_posy)
+        ctx.fillText('_',sub_posx+i*60,sub_posy)
     }
 }
 
@@ -38,7 +69,6 @@ function draw_gallows(){
     ctx.fillRect(gallows_posx,gallows_posy,150,10)
     ctx.fillRect(gallows_posx,gallows_posy,150,10)
     ctx.fillRect(gallows_posx+150,gallows_posy,10,40)
-
 }
 
 function draw_hangman(num){
@@ -77,5 +107,16 @@ function draw_hangman(num){
 }
 
 function send(){
-
+        let char = document.querySelector('#char').value
+        if(char == '')return alert('Digite algo.')
+        let check = false
+        for(let i=0;i<chosen_word.length;i++){
+            if(char == chosen_word[i]){
+                check = true
+                chars[i] = char
+            }
+        }
+        if(!check)num_error++
+        update()
+        return 0
 }
